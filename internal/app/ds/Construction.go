aeb156fe573bc4d16eedff9986f2b7a6119eaa1b
lexa <alexalexandrov2005@gmail.com>
2026-03-09 19:05:11 +0300

    add new branch

---
+++ internal/app/ds/Construction.go
@@ -0,0 +1,13 @@
+package ds
+
+type Construction struct {
+	ID                uint   `gorm:"primaryKey"`
+	ConstructionTitle string `gorm:"type:varchar(100);not null"`
+	UseLife           string `gorm:"type:varchar(50);not null"`
+	Description       string `gorm:"type:text;not null"`
+	ImageURL          string `gorm:"type:varchar(255)"`
+	VideoURL          string `gorm:"type:varchar(255)"`
+	CuttingDate       string `gorm:"type:varchar(20)"`
+	DateCorrection    string `gorm:"type:varchar(20)"`
+	IsDelete          bool   `gorm:"type:boolean;not null;default:false"`
+}
---
+++ internal/app/handler/handler.go
@@ -0,0 +1,40 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"metoda/internal/app/repository"
+)
+
+const minioBaseURL = "http://localhost:9090/constructions"
+
+type Handler struct {
+	Repository *repository.Repository
+}
+
+func NewHandler(r *repository.Repository) *Handler {
+	return &Handler{
+		Repository: r,
+	}
+}
+
+func (h *Handler) RegisterHandler(router *gin.Engine) {
+	router.GET("/", h.GetConstructions)
+	router.GET("/construction/:id", h.GetConstruction)
+	router.GET("/application", h.GetApplication)
+	router.POST("/add-to-application", h.AddToApplication)
+	router.POST("/delete-application", h.DeleteApplication)
+}
+
+func (h *Handler) RegisterStatic(router *gin.Engine) {
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./resources")
+}
+
+func (h *Handler) errorHandler(ctx *gin.Context, errorStatusCode int, err error) {
+	logrus.Error(err.Error())
+	ctx.JSON(errorStatusCode, gin.H{
+		"status":      "error",
+		"description": err.Error(),
+	})
+}
